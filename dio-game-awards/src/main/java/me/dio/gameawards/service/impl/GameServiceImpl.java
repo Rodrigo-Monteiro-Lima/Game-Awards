b5341cdaaaf665d9e8feb3d450048ecc6e463c8f
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-23 11:59:58 -0300

    feat: service implematation

---
+++ dio-game-awards/src/main/java/me/dio/gameawards/service/impl/GameServiceImpl.java
@@ -0,0 +1,50 @@
+package me.dio.gameawards.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import me.dio.gameawards.domain.model.Game;
+import me.dio.gameawards.domain.model.GameRepository;
+import me.dio.gameawards.service.GameService;
+
+@Service
+public class GameServiceImpl implements GameService{
+	
+	@Autowired
+	private GameRepository repository;
+	
+	@Override
+	public List<Game> findAll() {
+		List<Game> games = repository.findAll();
+		return games;
+	}
+
+	@Override
+	public Game findById(Long id) {
+		Optional<Game> game = repository.findById(id);
+		return null;
+	}
+
+	@Override
+	public void insert(Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Long id, Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
