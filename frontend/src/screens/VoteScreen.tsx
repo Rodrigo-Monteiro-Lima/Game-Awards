c7b01aa57c849fcd6423bb0eb2afe337b2e0e504
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-24 06:34:39 -0300

    feat: vote screen

--- frontend/src/screens/VoteScreen.tsx
+++ frontend/src/screens/VoteScreen.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const VoteScreen = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text>ypdateindo</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: '#fff',
+    backgroundColor: '#191919',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default VoteScreen;
