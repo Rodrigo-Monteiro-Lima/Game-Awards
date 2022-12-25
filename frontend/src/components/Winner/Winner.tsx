9b2ac87a3088eb71f013b7a1950fba424cf7cb58
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 07:33:18 -0300

    style: winner

--- frontend/src/components/Winner/Winner.tsx
+++ frontend/src/components/Winner/Winner.tsx
@@ -12,6 +12,40 @@ const Winner = () => {
       <Text style={styles.winnerLabel}>WINNER</Text>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color:'#fff',
+    backgroundColor: '#191919',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img:{
+    borderWidth:3,
+    borderColor:'#fff',
+    borderRadius:30
+  },
+  gameLabel:{
+    color:'#fff',
+    textAlign:'center',
+    fontSize:22
+  },
+  winnerLabel:{
+    backgroundColor:'#0EC244',
+    borderRadius:4,
+    textAlign:'center',
+    color:'#fff',
+    fontWeight:'bold',
+    fontSize:30,
+    paddig:10
+  },
+  card:{
+    borderRadius:10,
+    width: 200, 
+    height: 300,
+  }
+});
 
 export default Winner;
